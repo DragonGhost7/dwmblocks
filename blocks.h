516890f3d49002c5985a50995ed2fc8d1dfae9dc
DragonGhost7 <darkiridiumghost@gmail.com>
2020-06-14 13:09:55 -0400

    README.md update

--- blocks.h
+++ blocks.h
@@ -1,16 +1,16 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "~/.bin/memory",						5,		1},
-	{"", "~/.bin/cpu",							5,		1},
-	{"", "~/.bin/dwm_mail.sh",					6,		1},
-	{"", "~/.bin/dwm_battery.sh",				10,		1},
-	{"", "~/.bin/internet",					    3,		1},
-	{"", "~/.bin/dwm_date.sh",					1,		1},
-	{";", "~/.bin/dwm_keyboard.sh",				0,		1},
-	{"", "~/.bin/music",						0,		11},
-	{"", "~/.bin/volume",						0,		9},
-	{"", "~/.bin/weather",						3600,	12},
+	{"", "~/.bin/statusbar/memory",						5,		1},
+	{"", "~/.bin/statusbar/cpu",							5,		1},
+	{"", "~/.bin/statusbar/dwm_mail.sh",					6,		1},
+	{"", "~/.bin/statusbar/dwm_battery.sh",				10,		1},
+	{"", "~/.bin/statusbar/internet",					    3,		1},
+	{"", "~/.bin/statusbar/dwm_date.sh",					1,		1},
+	{";", "~/.bin/statusbar/dwm_keyboard.sh",				0,		1},
+	{"", "~/.bin/statusbar/music",						0,		11},
+	{"", "~/.bin/statusbar/volume",						0,		9},
+	{"", "~/.bin/statusbar/weather",						3600,	12},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
